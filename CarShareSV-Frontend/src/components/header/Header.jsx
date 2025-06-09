e0bedd598b2d0b93c54a7581adda889831f92318
AleMelgar <00026222@uca.edu.sv>
2025-06-08 19:25:22 -0600

    feature: Header and buttons

---
+++ CarShareSV-Frontend/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import Button from "../button/Button";
+
+export default function Header() {
+    return (
+        <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+            <div className="max-w-8xl mx-auto px-4 py-3 flex justify-between items-center">
+                <h1 className="text-xl font-bold text-primary">CarShareSV</h1>
+                <nav className="flex gap-4 mr-12">
+                    <Button href="">
+                        Registrarse
+                    </Button>
+                    <Button href="">
+                        Iniciar sesión
+                    </Button>
+                </nav>
+            </div>
+        </header>
+    );
+}
