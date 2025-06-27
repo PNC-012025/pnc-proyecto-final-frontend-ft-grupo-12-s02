743f48d4a2656b249381c121f7d990ac05558779
TrailWorksDev <diazmario4503@gmail.com>
2025-06-26 21:58:31 -0600

    dropzone feature added

--- CarShareSV-Frontend/src/services/cloudinary.service.js
+++ CarShareSV-Frontend/src/services/cloudinary.service.js
@@ -1,14 +1,22 @@
-export function sendImageToCloud (imageFormData) {
+export function sendImageToCloud(imageFormData) {
+  // LOG: Mostrar el contenido del FormData
+  for (let pair of imageFormData.entries()) {
+    console.log("Cloudinary formData:", pair[0], pair[1]);
+  }
+
   return fetch("https://api.cloudinary.com/v1_1/dqntcutk6/image/upload", {
     method: "POST",
     body: imageFormData
-  }).then(res => {
-    if(!res.ok) throw new Error("Error uploading image to cloudinary");
+  }).then(async res => {
+    // LOG: Mostrar status y respuesta
+    const data = await res.clone().json().catch(() => ({}));
+    console.log("Cloudinary response status:", res.status);
+    console.log("Cloudinary response body:", data);
 
-    return res.json();
+    if (!res.ok) throw new Error("Error uploading image to cloudinary");
+    return data;
   }).then(res => {
     const { secure_url } = res;
-    //console.log("IMAGE URL:" + secure_url);
     return secure_url;
   });
 }
