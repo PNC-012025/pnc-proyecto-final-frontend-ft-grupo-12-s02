1f0bb4d24c9ffd7380a1fac8cfb617331ef0806e
AleMelgar <00026222@uca.edu.sv>
2025-06-27 22:30:13 -0600

    feature: get rents

--- CarShareSV-Frontend/src/pages/actualRents/ActualRents.jsx
+++ CarShareSV-Frontend/src/pages/actualRents/ActualRents.jsx
@@ -6,42 +6,82 @@ import RentsSwitcher from '../../components/rentsSwitcher/RentsSwitcher';
 import useReservation from '../../hooks/useReservation';
 import useUser from '../../hooks/useUser';
 
+
+/* 
+[
+    {
+        "startDate": "2025-06-28",
+        "endDate": "2025-06-28",
+        "address": "Residencial Las Lomas, C. Los Girasoles, San Salvador",
+        "total": 95.0,
+        "status": "ACTIVE",
+        "reservingUsername": "YiToun",
+        "reservedCar": {
+            "carId": "3cc3e231-e98a-47f9-bdfd-baac800861c0",
+            "plateNumber": "P901906",
+            "description": "Tahoe 2019 de agencia, asientos de cuero, Tres filas de asientos, 4 capitan y tercera fila que se adapta y extiende baul.",
+            "visible": true,
+            "doors": 4,
+            "capacity": 7,
+            "dailyPrice": 95.0,
+            "location": "Residencial Las Lomas, C. Los Girasoles, San Salvador",
+            "year": 2019,
+            "username": "Mario45",
+            "model": "Tahoe",
+            "brand": "Chevrolet",
+            "transmission": "Automatic",
+            "images": [
+                "https://res.cloudinary.com/dqntcutk6/image/upload/v1751071811/carsharesv/cwboo1rmwh3ty1qk2hck.png",
+                "https://res.cloudinary.com/dqntcutk6/image/upload/v1751071812/carsharesv/jk4zlklrsqknt417zitk.png",
+                "https://res.cloudinary.com/dqntcutk6/image/upload/v1751071814/carsharesv/wpoha5iwoqoy2oxmtx0a.png",
+                "https://res.cloudinary.com/dqntcutk6/image/upload/v1751071815/carsharesv/t6hb5fpv37aq8s9xy21d.png",
+                "https://res.cloudinary.com/dqntcutk6/image/upload/v1751071816/carsharesv/ytv67shhnoh5vtktq7jz.png",
+                "https://res.cloudinary.com/dqntcutk6/image/upload/v1751071818/carsharesv/fxcgq0c7ugbbqnkool00.png"
+            ]
+        } */
+
 export default function ActualRents() {
     const { user } = useUser();
     const { userReservations, getUserReservations } = useReservation();
     const [activeReservations, setActiveReservations] = useState([]);
 
     useEffect(() => {
-      if (user && user.userId) {
-        getUserReservations(user.userId);
-
-        setActiveReservations(userReservations.filter(r => r.status === 'ACTIVE'));
-      }
-
-      console.log("Active Reservations: ", activeReservations);
+        if (user && user.userId) {
+            getUserReservations(user.userId);
+        }
     }, [user, getUserReservations]);
 
-    const cars = []
+    useEffect(() => {
+        if (userReservations && userReservations.length > 0) {
+            setActiveReservations(userReservations.filter(r => r.status === 'ACTIVE'));
+        }
+    }, [userReservations]);
+
+
 
     return (
         <div className="min-h-screen bg-gray-50">
             <Header />
             <div className="max-w-6xl mx-auto pt-28 pb-16 px-4 sm:px-6 lg:px-8">
-                <h2 className="text-4xl font-semibold text-gray-900 mb-12">
-                Publica tu vehículo
+                <h2 className="text-3xl font-semibold text-gray-900 mb-8">
+                    Mis rentas actuales
                 </h2>
-                
+
                 <RentsSwitcher />
 
                 <div className="space-y-10">
-                    {cars?.map((car) => (
-                        <div key={car.id} className="flex justify-between items-start gap-6">
-                            <div className="flex-1">
-                                <MyRentsCard car={car} />
+                    {activeReservations.map((activeReservation) => (
+                        <div key={activeReservation.reservedCar.carId} className="flex justify-between items-start gap-6">
+                            <div className='flex-1'>
+                                <MyRentsCard car={activeReservation.reservedCar} />
                             </div>
-                            <CurrentDetailCard car={car} />
+
+                           < CurrentDetailCard  rent={activeReservation}/>
+
                         </div>
+
                     ))}
+
                 </div>
 
             </div>
